6098f64f8bdc303f5e9e1db1e123fb8709fb3c86
Fescobar28 <franescobar97@hotmail.com>
2024-08-28 13:10:55 -0600

    Formulario y alerta creados

---
+++ src/components/Formulario.vue
@@ -0,0 +1,57 @@
+<script setup>
+  
+  import { reactive, ref } from 'vue';
+  import Alerta from './Alerta.vue';
+  const paises = [
+      { codigo: 'US', nombre: 'Estados Unidos' },
+      { codigo: 'MX', nombre: 'México' },
+      { codigo: 'AR', nombre: 'Argentina' },
+      { codigo: 'CO', nombre: 'Colombia' },
+      { codigo: 'CR', nombre: 'Costa Rica' },
+      { codigo: 'ES', nombre: 'España' },
+      { codigo: 'PE', nombre: 'Perú' }
+  ];
+
+  const busqueda = reactive({
+    ciudad: '',
+    pais: ''
+  });
+
+  const error = ref('');
+
+  const consultarClima = () => {
+    if(Object.values(busqueda).includes('')){
+        error.value = "Todos los campos son obligatorios";
+        setTimeout(()=>{error.value = ''}, 3000);
+        return;
+    }
+    console.log("Exito!")
+  }
+
+</script>
+
+<template>
+
+    <form class="formulario" @submit.prevent="consultarClima">
+    
+        <Alerta v-if="error">{{ error }}</Alerta>
+        
+        <div class="campo">
+          <label for="ciudad">Ciudad</label>
+          <input type="text" placeholder="Ciudad" id="ciudad" v-model="busqueda.ciudad">
+        </div>
+    
+        <div class="campo">
+          <label for="pais">Pais</label>
+          <select id="pais" v-model="busqueda.pais">
+            <option value="">-- Seleccione un país --</option>
+            <option v-for="pais in paises" :value="pais.codigo">{{ pais.nombre }}</option>
+          </select>
+        </div>
+    
+        <input type="submit" value="Consultar Clima">
+
+      </form>
+
+</template>
+

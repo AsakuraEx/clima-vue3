402705d853f6fb4b3324d31e646562a2ef0317e7
Fescobar28 <franescobar97@hotmail.com>
2024-09-02 13:26:39 -0600

    consumo de apis realizado

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,14 +1,29 @@
+import axios from 'axios';
+
 export default function useClima(){
     
-    const obtenerClima = ({ciudad, pais}) => {
+    const obtenerClima = async ({ciudad, pais}) => {
         
         //importando el api key
         const key = import.meta.env.VITE_API_KEY;
+        const ciudadApi = ciudad.replace(/ /g, '%');
 
         //Pasando latitud y longitud
-        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`;
-        console.log(url);
-        
+        try {
+
+            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudadApi},${pais}&limit=1&appid=${key}`;
+            
+            const { data } = await axios(url);
+            const { lat, lon } = data[0];
+
+            const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&lang=es`;
+            const {data: dataClima} = await axios(urlClima);
+            console.log(dataClima);
+                
+        } catch (error) {
+            console.log(error);
+        }
+
     }
     
     return {
